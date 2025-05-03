fe37838f7672e14adabc1e67b6772f34e8c9f601
Hubertas Vindžigalskis <53914627+HubertasVin@users.noreply.github.com>
2025-05-03 19:43:47 +0300

    Update Zone.cs

--- backend/Domain/Entities/Zone.cs
+++ backend/Domain/Entities/Zone.cs
@@ -3,8 +3,10 @@ public class Zone
     public Guid Id { get; init; }
     public string Name { get; private set; } = null!;
     public string Address { get; private set; } = null!;
-    public double Latitude { get; private set; }
-    public double Longitude { get; private set; }
+    public double Latitude1 { get; private set; }
+    public double Longitude1 { get; private set; }
+    public double Latitude2 { get; private set; }
+    public double Longitude2 { get; private set; }
     public int Capacity { get; private set; }
 
     public ICollection<Bike> Bikes { get; private set; } = new List<Bike>();
