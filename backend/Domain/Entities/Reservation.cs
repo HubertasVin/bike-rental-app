aec9802a94839011e2f62f42b6b206502ec8b0e6
Brigita Bruškytė <114769087+ClawOfNyx@users.noreply.github.com>
2025-05-17 20:59:05 +0300

    Updated domain Bike, Reservation and BikeStatus

--- backend/Domain/Entities/Reservation.cs
+++ backend/Domain/Entities/Reservation.cs
@@ -2,6 +2,16 @@ namespace BikeRentalApp.Domain.Entities;
 
 public class Reservation
 {
+    protected Reservation() { }
+
+    public Reservation(Guid id, Guid userId, Guid bikeId, DateTime startTime)
+    {
+        Id = id;
+        UserId = userId;
+        BikeId = bikeId;
+        StartTime = startTime;
+    }
+
     public Guid Id { get; init; }
 
     public Guid UserId { get; private set; }
@@ -11,5 +21,10 @@ public class Reservation
     public Bike Bike { get; private set; } = null!;
 
     public DateTime StartTime { get; private set; }
-    public DateTime ExpirationTime { get; private set; }
+    public DateTime? EndTime { get; private set; }
+
+    public void End()
+    {
+        EndTime = DateTime.UtcNow;
+    }
 }
